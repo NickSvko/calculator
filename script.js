63044fbb04828883f231b300f40ea9de3e270b35
Nick Savchenko <NickSvko@gmail.com>
2023-12-11 16:32:02 +0200

    Add operator() function

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+let firstOperand, secondOperand, operator;
+
+
+function operate(firstOperand, secondOperand, operator) {
+    switch(operator) {
+        case '+':
+            add(firstOperand, secondOperand);
+            break;
+        case '−':
+            subtrant(firstOperand, secondOperand);
+            break;
+        case '×':
+            multiply(firstOperand, secondOperand);
+            break;        
+        case '÷':
+            divide(firstOperand, secondOperand);
+            break;
+        case '%':
+            module(firstOperand, secondOperand);
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -11,5 +45,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    return num1 / num2;
+}
 
+function module(num1, num2) {
+    return num1 % num2;
 }
